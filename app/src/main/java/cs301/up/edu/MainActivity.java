dabf2d2be5b9c6dcfa4e149a31322885c02bfebf
torkelso19 <torkelso19@up.edu>
2019-01-25 10:43:16 -0800

    Removed Multiple Interface Bars

--- app/src/main/java/cs301/up/edu/MainActivity.java
+++ app/src/main/java/cs301/up/edu/MainActivity.java
@@ -2,6 +2,8 @@ package cs301.up.edu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * External Citation
+         *  Date:        1/25/19
+         *  Problem:     Wanted to get rid of status bars
+         *  Resource:    https://developer.android.com/training/system-ui/navigation#java
+         *  Solution:    Used code from website and read possible view options to add immersive
+         */
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }
