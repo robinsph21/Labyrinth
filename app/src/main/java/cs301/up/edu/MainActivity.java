5bf54c2acd73ba75866af8e8960a04df9d658e90
torkelso19 <torkelso19@up.edu>
2019-02-06 20:24:32 -0800

    minor change

--- app/src/main/java/cs301/up/edu/MainActivity.java
+++ app/src/main/java/cs301/up/edu/MainActivity.java
@@ -3,7 +3,6 @@ package cs301.up.edu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import cs301.up.edu.xmlObjects.CurrentTile;
