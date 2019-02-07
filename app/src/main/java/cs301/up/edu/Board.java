3c3175aad01e13c308c782aa76b63de0b7092ecb
Spencer <nelsons19@up.edu>
2019-02-06 18:04:31 -0800

    Added clarification comments to Board constructor

--- app/src/main/java/cs301/up/edu/Board.java
+++ app/src/main/java/cs301/up/edu/Board.java
@@ -9,10 +9,54 @@ public class Board {
     private static final int LENGTH = 10;
     private BoardSpot[][] gameBoard = new BoardSpot[LENGTH][LENGTH];
 
+    /**
+     * Constructor for our board. Our board is a 9x9 grid. All tiles on the outside of the grid are
+     * of type BoardEdge, all other cells are of type BoardTile. BoardTiles and BoardEdges have
+     * different properties based on their position. The below logic initializes the properties of
+     * cells to the correct values.
+     * In the below diagram, cell status can been determined based on the following legend.
+     *
+     * LEGEND
+     * |   | - Denotes BoardTiles
+     * | S | - Denotes stationary BoardTiles
+     * {   } - Denotes BoardEdges
+     * { C } - Denotes clickable BoardEdges
+     *
+     *  (0,0)
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      {   } {   } { C } {   } { C } {   } { C } {   } {   }
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      {   } | S | |   | | S | |   | | S | |   | | S | {   }
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      { C } |   | |   | |   | |   | |   | |   | |   | { C }
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      {   } | S | |   | | S | |   | | S | |   | | S | {   }
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      { C } |   | |   | |   | |   | |   | |   | |   | { C }
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      {   } | S | |   | | S | |   | | S | |   | | S | {   }
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      { C } |   | |   | |   | |   | |   | |   | |   | { C }
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      {   } | S | |   | | S | |   | | S | |   | | S | {   }
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *      {   } {   } { C } {   } { C } {   } { C } {   } {   }
+     *      ----- ----- ----- ----- ----- ----- ----- ----- -----
+     *                                                          (9,9)
+     */
     public Board() {
         for (int i = 1; i < 10; i++) {
             for (int j = 1; j < 10; j++) {
                 if (i == 1 || i == 9) {
+                    /*Make appropriate top and bottom rows BoardEdge clickable*/
                     switch (j) {
                         case 3:
                             gameBoard[i][j] = new BoardEdge(i, j, true);
@@ -25,6 +69,7 @@ public class Board {
                     }
                 } else if (j == 1 || j == 9) {
                     switch (i) {
+                        /*Make appropriate left and right columns BoardEdge clickable*/
                         case 3:
                             gameBoard[i][j] = new BoardEdge(i, j, true);
                         case 5:
@@ -36,7 +81,7 @@ public class Board {
                     }
                 }
                 else {
-                    //Check if tile should be fixed based on position
+                    /*Make appropriate BoardTiles fixed*/
                     if ((i % 2 == 0) && (j % 2 == 0)) {
                         gameBoard[i][j] = new BoardTile(i, j, true);
                     } else {
